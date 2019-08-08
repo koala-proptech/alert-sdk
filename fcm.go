efeda6c371f1c8ea3f3ce12bedaae7dc03a6ff10
Wibi Aristama <wibi.aristama@gmail.com>
2019-08-08 16:42:56 +0700

    fix url

--- fcm.go
+++ fcm.go
@@ -15,6 +15,6 @@ type (
 )
 
 func (c *Client) MultipleDevice(ctx context.Context, req MultipleDeviceRequest) (*Response, error) {
-	url := fmt.Sprintf("%s//push-notif/send", c.url)
+	url := fmt.Sprintf("%s/push-notif/send", c.url)
 	return c.walk(http.MethodPost, url, c.token, req)
 }
