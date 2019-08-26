d858ee1d18d0cc5cf70c11c174d52855646a84a5
Wibi Aristama <wibi.aristama@gmail.com>
2019-08-27 00:00:21 +0700

    added send mail with attachment in sdk

--- email.go
+++ email.go
@@ -3,15 +3,19 @@ package sdk
 import (
 	"context"
 	"fmt"
+	"io"
 	"net/http"
 )
 
 type (
 	EmailRequest struct {
-		From    string      `json:"from"`
-		To      []string    `json:"to"`
-		Subject string      `json:"subject"`
-		Body    interface{} `json:"body"`
+		From               string        `json:"from"`
+		To                 []string      `json:"to"`
+		Subject            string        `json:"subject"`
+		Body               interface{}   `json:"body"`
+		Text               string        `json:"text"`
+		Attachment         io.ReadCloser `json:"attachment"`
+		AttachmentFileName string        `json:"attachment_filename"`
 	}
 )
 
@@ -19,3 +23,10 @@ func (c *Client) VerificationEmail(ctx context.Context, req EmailRequest) (*Resp
 	url := fmt.Sprintf("%s/email/verification", c.url)
 	return c.walk(http.MethodPost, url, c.token, req)
 }
+
+func (c *Client) WithAttachment(ctx context.Context, req EmailRequest) (*Response, error) {
+	url := fmt.Sprintf("%s/email/with-attachment", c.url)
+	return c.walk(http.MethodPost, url, c.token, req)
+}
+
+
